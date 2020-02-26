f3bd5271d462f3ad3076cf121918237028562eae
Greczu <61417094+Greczu@users.noreply.github.com>
2020-02-27 00:22:51 +0100

    Update MD_Blackjack.py

--- MD_Blackjack.py
+++ MD_Blackjack.py
@@ -1,12 +1,46 @@
+import time
 from os import system
+from random import seed
 from random import randint
+
+seed(time.time())
+
+figura = ["As", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Walet", "Dama", "Król"]
+kolor = ["Pik", "Kier", "Trefl", "Karo"]
+taliaGracza = []
+taliaBota = []
+noRepetition = False
+
+def takeCard(talia):
+    while noRepetition:
+        cardNr = figura[randint(0,12)]
+        cardColor = kolor[randint(0,3)]
+        for 
+    talia.append(cardNr + cardColor)
+
 
 def botAction(whoStarts):
     if(whoStarts == "2"):
-        print("No gram")
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        takeCard(taliaBota)
+        print(taliaBota)
         return
     else:
-        return 
+        return
 
 while True:
 

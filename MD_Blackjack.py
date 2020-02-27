fd3551257da58ffab66faecaea9d9216a6ebbecd
Greczu <61417094+Greczu@users.noreply.github.com>
2020-02-27 23:16:13 +0100

    Update MD_Blackjack.py

--- MD_Blackjack.py
+++ MD_Blackjack.py
@@ -10,40 +10,49 @@ kolor = ["Pik", "Kier", "Trefl", "Karo"]
 taliaGracza = []
 taliaBota = []
 noRepetition = False
+botPoints = 0
+playerPoints = 0
 
-def takeCard(talia):
+def addPoints(card):
+    if (card == "As"):
+        return 11
+    elif (card == "Walet"):
+        return 10
+    elif (card == "Dama"):
+        return 10
+    elif (card == "Król"):
+        return 10
+    else:
+        return int(card)
+
+def takeCard(talia, sumOfPoints):
+    noRepetition = 1
+    cardNr = ""
+    cardColor = ""
+
     while noRepetition:
+        noRepetition = 0
         cardNr = figura[randint(0,12)]
         cardColor = kolor[randint(0,3)]
-        for 
-    talia.append(cardNr + cardColor)
+        choosenCard = cardNr + " " + cardColor
+        for i in taliaBota:
+            if(choosenCard == i):
+                noRepetition = 1
 
+        for i in taliaGracza:
+            if(choosenCard == i):
+                noRepetition = 1
+    talia.append(choosenCard)
+    return sumOfPoints + addPoints(cardNr)
 
-def botAction(whoStarts):
-    if(whoStarts == "2"):
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        takeCard(taliaBota)
-        print(taliaBota)
-        return
+def botAction(sumOfPoints):
+    if (sumOfPoints < 18):
+        sumOfPoints = takeCard(taliaBota, sumOfPoints)
+        return 0, sumOfPoints
     else:
-        return
+        return 1, sumOfPoints
 
 while True:
-
     print ("Kto zaczyna?\n1. Ty\n2. Bot")
     whoStarts = input()
     if (whoStarts != "1" and whoStarts != "2"):
@@ -52,6 +61,54 @@ while True:
     else:
         break
 
-system('cls')
+passBot = 0
+passPlayer = 0
 
-botAction(whoStarts)
+if(whoStarts == "2"):
+    pasBot, botPoints = botAction(botPoints)
+
+while True:
+    system('cls')
+    print ("Bot: ")
+    print(taliaBota)
+    print(" Suma: " + str(botPoints))
+    print ("Gracz: ")
+    print(taliaGracza)
+    print(" Suma: " + str(playerPoints))
+
+    if (playerPoints > 21):
+        print("Przegrana")
+        break
+
+    if (botPoints > 21):
+        print("Wygrana")
+        break
+
+    if (passPlayer == 1 and passBot == 1):
+        if(botPoints > playerPoints):
+            print("Przegrana")
+            break
+        elif(playerPoints > botPoints):
+            print("Wygrana")
+            break
+        else:
+            print("Remis")
+            break
+
+    print ("Wybierz akcję:\n1. Dobierz kartę\n2. Pas")
+
+    while True:
+        if(passPlayer == 0):
+            chose = input()
+            if(chose == "1"):
+                playerPoints = takeCard(taliaGracza, playerPoints)
+                break
+            elif (chose == "2"):
+                passPlayer = 1
+                break
+            else:
+                print("Zły znak!")
+        else:
+            break
+
+    passBot, botPoints = botAction(botPoints)
